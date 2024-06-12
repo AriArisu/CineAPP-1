bb80b9fbf03d710a822464d3b055d400822e24fd
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-12 09:33:16 -0300

    pagina de detelhes

--- APP/src/main.jsx
+++ APP/src/main.jsx
@@ -7,6 +7,7 @@ import Filmes from './pages/Filmes.jsx'
 import Sobre from './pages/Sobre.jsx'
 import Contato from './pages/Contato.jsx'
 import PageNotFound from './pages/PageNotFound.jsx'
+import DetalhesFilme from './pages/DetalhesFilme.jsx'
 import Home from './pages/Home.jsx'
 
 const router = createBrowserRouter(
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
       children:[
         {index: true, element: <Home/>},
         { path: "filmes", element: <Filmes/>},
+        {path: "filmes/:id", element: <DetalhesFilme/>},
         { path: "sobre", element: <Sobre/>},
         { path: "contato", element: <Contato/>},
         { path: "*", element: <PageNotFound/>}
