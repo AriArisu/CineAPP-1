ae0cb0fc54a2e105bc73eddb2e9efa3810f9a436
LeticiaGiVi <leticia.villegas@outlook.com>
2024-06-25 19:43:30 -0300

    AC

--- APP/src/App.jsx
+++ APP/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-    <div className="min-h-screen flex flex-col justify-between">
+    <div className="min-h-screen flex flex-col justify-between items-center">
     <Header/>
     <Outlet/>
     </div>
--- APP/src/pages/Home.jsx
+++ APP/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import data from '../../artigos.json'
 
 function Home() {
-    const destaques = data.slice(0, 6);
+    const maisNova = data.slice(0,1);
+    const destaques = data.slice(1, 6);
 
     return ( 
         <>
-            <div className="grid grid-cols-3">
-                {destaques.map((filme, index) => (
-                    <div className="card m-2" key={index}>
+            <div className="noticiasNovas flex flex-row w-9/12 items-between">
+            {maisNova.map((filme, index) => (
+                    <div className="Noticiaprincipal card m-2 w-1/3" key={index}>
                         <h1>{filme.title}</h1>
                         {filme.image ? (
                             <img src={filme.image} alt={filme.title} />
@@ -26,6 +27,55 @@ function Home() {
                         </div>
                     </div>
                 ))}
+                <div className='flex flex-col'>
+                {destaques.map((filme, index) => (
+                    <div className="MaisNoticias card m-2 w-1/5" key={index}>
+                        {filme.image ? (
+                            <img src={filme.image} alt={filme.title} />
+                        ) : (
+                            <h1 className='text-4xl'>Não tem imagem</h1>
+                        )}
+                        <div>
+                        <h1>{filme.title}</h1>
+                        <div className='tags'>
+                            {filme.tags.map(tag => (
+                                <span className='bg-pink-800 text-white p-1 m-1' key={tag}>{tag}</span>
+                            ))}
+                        </div>
+                        </div>
+                    </div>
+                ))}
+                </div>
+            </div>
+
+            <div className='noticiasGerais flex flex-row flex-wrap w-10/12'>
+            {data.map((filme, index) => (
+                    <div className="card m-2 w-1/4" key={index}>
+                        <h1>{filme.title}</h1>
+                        {filme.image ?
+                            <img src={filme.image}></img>
+                            :
+                            <h1 className='text-4x1'>Não tem imagem</h1>
+                        }
+                        <div className='tags'>
+                        {
+                            filme.tags.map( tag =>(
+                                <span className='bg-pink-800 text-white p-1 m-1' key={tag}>{tag}</span>
+                            ))
+                        }
+                        </div>
+                        <div className='texto'>
+                        {
+                            filme.text.map( texto =>(
+                                <p key={texto}>{texto}</p>
+                            )
+                            )
+                        }
+                        </div>
+                    </div>
+                )
+            )
+        }
             </div>
         </>
      );
